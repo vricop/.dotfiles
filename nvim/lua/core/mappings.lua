9e314abb5f34ef924939e507d7109affc0b0ae16
Víctor Rico <vricop@gmail.com>
2022-12-17 20:11:03 +0100

    nvim: Add new mappings
    - Improve J command (join lines)
    - Improve vertical navigation jumps
    - Add the possibility to move line up & down

--- nvim/lua/core/mappings.lua
+++ nvim/lua/core/mappings.lua
@@ -3,9 +3,21 @@ local keymap = vim.keymap.set
 keymap("n", "<Leader>h", ":nohlsearch<cr>", { silent = true }) -- Stop highlighting
 keymap("n", "<C-s>", ":w<cr>", { silent = true }) -- Save file
 
+-- Improved "join lines". It sets a mark at "z" so we can move back to that
+-- position after each "J" keypress. I took this from ThePrimeagen
+keymap("n", "J", "mzJ`z")
+
 -- Tab navigation
 keymap("n", "<S-h>", "<Esc>:tabprevious<cr>", { silent = true })
 keymap("n", "<S-l>", "<Esc>:tabnext<cr>", { silent = true })
+
+-- Move lines up/down
+keymap("v", "<C-j>", ":m '>+1<Cr>gv=gv")
+keymap("v", "<C-k>", ":m '>-2<Cr>gv=gv")
+
+-- Better vertical navigation jumps
+keymap("n", "<C-d>", "<C-d>zz")
+keymap("n", "<C-u>", "<C-u>zz")
 
 -- Better window navigation
 keymap("n", "<C-h>", "<C-w>h")
