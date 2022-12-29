e8aa70141a23e41c1bf682db1a84f15622d850d9
Víctor Rico <vricop@gmail.com>
2022-12-29 19:13:42 +0100

    nvim: Update neovim ascii art comment header

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,10 +1,11 @@
--- `7MN.   `7MF'                             db
---   MMN.    M
---   M YMb   M  .gP"Ya   ,pW"Wq.`7M'   `MF'`7MM  `7MMpMMMb.pMMMb.
---   M  `MN. M ,M'   Yb 6W'   `Wb VA   ,V    MM    MM    MM    MM
---   M   `MM.M 8M"""""" 8M     M8  VA ,V     MM    MM    MM    MM
---   M     YMM YM.    , YA.   ,A9   VVV      MM    MM    MM    MM
--- .JML.    YM  `Mbmmd'  `Ybmd9'     W     .JMML..JMML  JMML  JMML.
+--
+-- ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
+-- ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
+-- ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
+-- ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
+-- ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
+-- ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝
+--
 
 require 'core.options'
 require 'core.mappings'
