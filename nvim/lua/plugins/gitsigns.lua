3b0c3035ab4d5bed56330f99de621f8e85167791
Víctor Rico <vricop@gmail.com>
2023-02-23 17:29:08 +0100

    nvim: Add mappings for gitsigns

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -2,7 +2,19 @@ return {
   {
     'lewis6991/gitsigns.nvim',
     config = function()
-      require('gitsigns').setup()
-    end
+      local gitsigns = require'gitsigns'
+
+      gitsigns.setup({
+        on_attach = function()
+          local map = vim.keymap.set
+
+          map( 'n', ']h',         gitsigns.next_hunk,       { desc = 'Go next hunk' })
+          map( 'n', '[h',         gitsigns.prev_hunk,       { desc = 'Go prev hunk' })
+          map( 'n', '<Leader>gp', gitsigns.preview_hunk,    { desc = 'Preview hunk' })
+          map( 'n', '<Leader>gu', gitsigns.undo_stage_hunk, { desc = 'Undo hunk' })
+          map( 'n', '<Leader>gr', gitsigns.reset_buffer,    { desc = 'Reset buffer' })
+        end
+      })
+    end,
   }
 }
