2ffab368e97c5485e074b59931853ec7da0c4d6d
Víctor Rico <vricop@gmail.com>
2023-03-03 21:22:37 +0100

    nvim: Customize Telescope icons & mappings

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -5,18 +5,43 @@ return {
     config = function()
       require 'telescope'.setup {
         defaults = {
-          prompt_prefix = '   '
+          selection_caret = ' ',
+          prompt_prefix = '   ',
+          sorting_strategy = 'ascending',
+          layout_config = {
+            prompt_position = 'top'
+          }
         },
         pickers = {
-          find_files = {
-            mappings = {
-              i = {
-                ['<Cr>'] = 'select_tab',
-              },
-            },
+          live_grep = {
+            prompt_prefix = " 󰑑 "
           },
+          grep_string = {
+            prompt_prefix = "   "
+          },
+          keymaps = {
+            prompt_prefix = " 󰌌  "
+          },
+          marks = {
+            prompt_prefix = " 󰃀 "
+          },
+          oldfiles = {
+            prompt_title = "Recent files",
+            prompt_prefix = "   "
+          }
         },
       }
+      local map = vim.keymap.set
+      map("n", "<Leader>ff", "<Cmd>Telescope find_files<Cr>", { desc = "Find Files" })
+      map("n", "<Leader>fr", "<Cmd>Telescope oldfiles<Cr>", { desc = "Find Recent Files" })
+      map("n", "<Leader>fk", "<Cmd>Telescope keymaps<Cr>", { desc = "Find Keymaps" })
+      map("n", "<Leader>f\"", "<Cmd>Telescope registers<Cr>", { desc = "Find Registers" })
+      map("n", "<Leader>f`", "<Cmd>Telescope marks<Cr>", { desc = "Find Marks" })
+      map("n", "<Leader>fw", "<Cmd>Telescope grep_string<Cr>", { desc = "Find Word Under Cursor" })
+      map("n", "<Leader>ft", "<Cmd>Telescope live_grep<Cr>", { desc = "Find Text In Project" })
+      map("n", "<Leader>fb", "<Cmd>Telescope buffers<Cr>", { desc = "Find buffers" })
+      map("n", "<Leader>fgb", "<Cmd>Telescope git_branches<Cr>", { desc = "Find git branches" })
+      map("n", "<Leader>fgc", "<Cmd>Telescope git_commits<Cr>", { desc = "Find git commits" })
     end,
   },
 }
