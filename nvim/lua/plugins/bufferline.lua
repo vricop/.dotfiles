b5fb90fbfdcd1ff1358af26da0a2e1d2796b215f
Víctor Rico <vricop@gmail.com>
2023-03-03 15:58:52 +0100

    nvim: Add mappings for bufferline

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -5,6 +5,16 @@ return {
     dependencies = {{ 'nvim-tree/nvim-web-devicons' }},
     config = function()
       require 'bufferline'.setup {}
+      local map = vim.keymap.set
+      -- Remap default (fallback & native) tab mappings
+      map(
+        'n', '<S-h>', '<Cmd>BufferLineCyclePrev<Cr>',
+        { desc = 'Move prev tab' }
+      )
+      map(
+        'n', '<S-l>', '<Cmd>BufferLineCycleNext<Cr>',
+        { desc = 'Move next tab' }
+      )
     end
   }
 }
