446610e97d0f512c07f22d8dcadeb33b67db63c7
Víctor Rico <vricop@gmail.com>
2023-03-03 21:23:34 +0100

    nvim: Install toggleterm plugin

---
+++ nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    'akinsho/toggleterm.nvim',
+    version = "*",
+    opts = {
+      direction = 'float',
+      border = "shadow",
+      size = 20,
+      open_mapping = [[<C-t>]],
+    },
+  }
+}
