2ab53441137d0e394c6649a47149899fb89cb10b
Víctor Rico <vricop@gmail.com>
2023-03-03 21:23:12 +0100

    nvim: Update whichkey grouping names

--- nvim/lua/plugins/whichkey.lua
+++ nvim/lua/plugins/whichkey.lua
@@ -4,13 +4,26 @@ return {
     config = function()
       vim.o.timeout = true
       vim.o.timeoutlen = 300
-      require("which-key").setup({
+      local which_key = require 'which-key'
+      which_key.setup {
         icons = {
           breadcrumb = "",
           separator = "— ",
           group = "",
         },
-      })
+      }
+
+    -- Only add label for groups, descriptions for mapping are set right where
+    -- they're defined
+    which_key.register {
+      ["<Leader>f"] = { name = "Find" },
+      ["<Leader>fg"] = { name = "Find git" },
+      ["<Leader>g"] = { name = "Git" },
+      ["<Leader>h"] = { name = "Hunk" },
+      ["<Leader>l"] = { name = "Lsp" },
+      ["<Leader>w"] = { name = "Workspace" },
+      ["<Leader>t"] = { name = "Toggle" },
+    }
     end,
   },
 }
