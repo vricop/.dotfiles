7141c11c2eeb3e46a7328b7f1397eae7eb89d784
Víctor Rico <vricop@gmail.com>
2023-03-08 16:02:47 +0100

    nvim: Update nvim config
    - Normalize string format in config file
    - Add custom mapping for navigating diagnostics. Match LSP mappings.
    - Add filter icon

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,26 +1,43 @@
- return {
-  {
-    'nvim-tree/nvim-tree.lua',
-    dependencies = {{ 'nvim-tree/nvim-web-devicons' }},
-    config = function()
-      require('nvim-tree').setup({
+return {
+	{
+		'nvim-tree/nvim-tree.lua',
+		dependencies = { { 'nvim-tree/nvim-web-devicons' } },
+		config = function()
+      require('nvim-tree').setup {
+        on_attatch = function(bufnr)
+          local api = require 'nvim-tree.api'
+          local map = vim.keymap.set
+          local function opts(desc)
+            return {
+              desc = 'Nvim-tree: ' .. desc,
+              buffer = bufnr,
+              noremap = true,
+              silent = true,
+              nowait = true,
+            }
+          end
+          -- Match LSP diagnostics mappings
+          map('n', ']d', api.node.navigate.diagnostics.next, opts('Next Diagnostic'))
+          map('n', '[d', api.node.navigate.diagnostics.prev, opts('Prev Diagnostic'))
+        end,
         view = {
           mappings = {
             custom_only = false,
             list = {
-              { key = { "l", "<CR>", "o" }, action = "edit", mode = "n" },
-              { key = "h", action = "close_node" },
-              { key = "v", action = "vsplit" },
-              { key = "C", action = "cd" },
+              -- TODO: Move to `on_attatch` field. Check LuanarVim nvim mappings
+              { key = { 'l', '<CR>', 'o' }, action = 'edit', mode = 'n' },
+              { key = 'h', action = 'close_node' },
+              { key = 'v', action = 'vsplit' },
+              { key = 'C', action = 'cd' },
             },
           },
         },
         renderer = {
-          root_folder_label = ":t",
+          root_folder_label = ':t',
           highlight_git = true,
           icons = {
-            git_placement = "after",
-            padding = " ",
+            git_placement = 'after',
+            padding = ' ',
             show = {
               file = true,
               folder = true,
@@ -28,46 +45,49 @@
               git = true,
               modified = true,
             },
-            -- Update broken icons after nerdfont patch is updated for PragmataPro
             glyphs = {
-              default = "",
-              symlink = "",
-              bookmark = "",
-              modified = "●",
+              default = '',
+              symlink = '',
+              bookmark = '',
+              modified = '●',
               folder = {
-                arrow_closed = "",
-                arrow_open = "",
-                default = "󰉋",
-                open = "󰝰",
-                empty = "󰉖",
-                empty_open = "ﱮ",
-                symlink = "󱧱",
-                symlink_open = "ﱮ",
+                arrow_closed = '',
+                arrow_open = '',
+                default = '󰉋',
+                open = '󰝰',
+                empty = '󰉖',
+                empty_open = 'ﱮ',
+                symlink = '󱧱',
+                symlink_open = 'ﱮ',
               },
               git = {
-                unstaged = "●",
-                staged = "",
-                unmerged = "",
-                renamed = "R",
-                untracked = "U",
-                deleted = "D",
-                ignored = " ",
+                unstaged = '●',
+                staged = '',
+                unmerged = '',
+                renamed = 'R',
+                untracked = 'U',
+                deleted = 'D',
+                ignored = ' ',
               },
             },
           },
           special_files = {
-            "Cargo.toml", "Makefile", "README.md", "readme.md", "Dockerfile"
+            'Cargo.toml',
+            'Makefile',
+            'README.md',
+            'readme.md',
+            'Dockerfile',
           },
         },
-      live_filter = {
-        prefix = "FILTER: ",
-      },
-    })
+        live_filter = {
+          prefix = '󰈶 FILTER : ',
+        },
+      }
 
-    vim.keymap.set('n', '<Leader>e', ':NvimTreeToggle<Cr>', {
-      desc = 'Toggle explorer',
-      silent = true
-    })
-  end
-  }
+			vim.keymap.set('n', '<Leader>e', ':NvimTreeToggle<Cr>', {
+				desc = 'Toggle explorer',
+				silent = true,
+			})
+		end,
+	},
 }
