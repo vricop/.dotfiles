2936d55b727c8794123880a062fcf7e5bd90e0e1
Víctor Rico <vricop@gmail.com>
2023-03-08 16:13:38 +0100

    nvim: Add null_lsp & typescript code actions plugin

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,7 +1,23 @@
 return {
   {
+    'jose-elias-alvarez/null-ls.nvim',
+    dependencies = { 'jose-elias-alvarez/typescript.nvim' },
+    event = { 'BufReadPre', 'BufNewFile' },
+    opts = function()
+      local null_ls = require 'null-ls'
+      return {
+        sources = {
+          null_ls.builtins.formatting.stylua,
+          null_ls.builtins.formatting.eslint,
+          null_ls.builtins.formatting.prettier,
+          require'typescript.extensions.null-ls.code-actions'
+        },
+      }
+    end,
+  },
+  {
     'neovim/nvim-lspconfig',
-    dependencies = { "jose-elias-alvarez/typescript.nvim" },
+    dependencies = { 'folke/neodev.nvim' },
     config = function ()
       vim.diagnostic.config({
         virtual_text = false,
