9a7fd5c72e7610b76b4dc51e97d5428dbaea404c
Víctor Rico <vricop@gmail.com>
2023-03-08 18:20:15 +0100

    nvim: Customize todo plugin icons

--- nvim/lua/plugins/todo-comments.lua
+++ nvim/lua/plugins/todo-comments.lua
@@ -1,10 +1,24 @@
 return {
   {
     {
-      "folke/todo-comments.nvim",
-      requires = "nvim-lua/plenary.nvim",
+      'folke/todo-comments.nvim',
+      requires = 'nvim-lua/plenary.nvim',
       config = function()
-        require'todo-comments'.setup { }
+        require 'todo-comments'.setup {
+          keywords = {
+            FIX = {
+              icon = ' ',
+              color = 'error',
+              alt = { 'FIXME', 'BUG', 'FIXIT', 'ISSUE' },
+            },
+            TODO = { icon = '󰸞 ', color = 'info' },
+            HACK = { icon = ' ', color = 'warning' },
+            WARN = { icon = ' ', color = 'warning', alt = { 'WARNING', 'XXX' } },
+            PERF = { icon = '󰾆 ', alt = { 'OPTIM', 'PERFORMANCE', 'OPTIMIZE' } },
+            NOTE = { icon = ' ', color = 'hint', alt = { 'INFO' } },
+            TEST = { icon = '󰙨 ', color = 'test', alt = { 'TESTING', 'PASSED', 'FAILED' } },
+          }
+        }
       end
     }
   }
