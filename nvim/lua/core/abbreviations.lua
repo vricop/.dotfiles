5a3b699bb5c59d8ab82fe3791f387824cfad27d6
Víctor Rico <vricop@gmail.com>
2023-03-12 22:29:06 +0100

    nvim: Update abbreviations

--- nvim/lua/core/abbreviations.lua
+++ nvim/lua/core/abbreviations.lua
@@ -1,8 +1,7 @@
 vim.cmd [[
-iabb cl console.log()<Esc>i
-iabb ct console.table()<Esc>i
-iabb cd console.dir()<Esc>i
-
-" Multiline column CSS/TS
-iabb ** /*<Cr><Cr>/<Esc>2ka*<Esc>ja<Space>
+  iabb html HTML
+  iabb css CSS
+  iabb JS JavaScript
+  iabb GH GitHub
+  iabb TODO /*<Cr>TODO: 
 ]]
