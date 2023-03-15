d188a8dc40e60e69aa48af26b4ae30ff0a6333fb
Víctor Rico <vricop@gmail.com>
2023-03-15 08:20:56 +0100

    nvim: Change mapping for moving code up/down

--- nvim/lua/core/mappings.lua
+++ nvim/lua/core/mappings.lua
@@ -6,8 +6,8 @@ map("n", "<Leader>S", ":wa<Cr>", { desc = "Save buffers", silent = true })
 map("n", "J", "mzJ`z", { desc = "Join lines and center screen" }) -- From ThePrimeagen
 map("n", "<S-h>", "<Esc>:tabprevious<cr>", { desc = "Move previous tab", silent = true })
 map("n", "<S-l>", "<Esc>:tabnext<cr>", { desc = "Move next tab", silent = true })
-map("v", "<C-j>", ":m '>+1<Cr>gv=gv", { desc = "Move line up" })
-map("v", "<C-k>", ":m '<-2<Cr>gv=gv", { desc = "Move line down" })
+map("v", "<S-j>", ":m '>+1<Cr>gv=gv", { desc = "Move line up" })
+map("v", "<S-k>", ":m '<-2<Cr>gv=gv", { desc = "Move line down" })
 map("n", "<Tab>", ">>", { desc = "Shift line right" })
 map("n", "<S-Tab>", "<<", { desc = "Shift line left" })
 map("v", "<Tab>", ">gv", { desc = "Shift line right" })
@@ -33,7 +33,7 @@ if not is_telescope_loaded then
   -- "Native" fuzzy finder (Set `vim.opt.path:append "**"` for better results)
   map("n", "<Leader>ff", function()
     -- If empty buffer, reuse it
-    if vim.fn.bufname("%") == "" then
+    if vim.fn.bufname(0) == "" then
       return ":find<Space>"
     end
 
