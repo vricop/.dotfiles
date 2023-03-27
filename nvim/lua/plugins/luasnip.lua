1bae2cb5b157fba16dbe3612b2504f7d0c9cf9f5
Víctor Rico <vricop@gmail.com>
2023-03-27 16:47:09 +0200

    nvim: Update plugins

--- nvim/lua/plugins/luasnip.lua
+++ nvim/lua/plugins/luasnip.lua
@@ -1,9 +1,6 @@
 return {
   {
     "L3MON4D3/LuaSnip",
-    -- follow latest release.
-    version = "<CurrentMajor>.*",
-    -- install jsregexp (optional!).
     build = "make install_jsregexp"
   }
 }
