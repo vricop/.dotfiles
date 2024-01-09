ca6914fd0522981ffcb709ed4dc1fbcbbf8dfdc9
Víctor Rico <vricop@gmail.com>
2024-01-09 11:04:22 +0100

    nvim: Update bufferline plugin config

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -1,16 +1,16 @@
 return {
   {
     'akinsho/bufferline.nvim',
-    version = "v3.*",
-    dependencies = {{ 'nvim-tree/nvim-web-devicons' }},
+    dependencies = { { 'nvim-tree/nvim-web-devicons' } },
     config = function()
-      require 'bufferline'.setup {}
+      require 'bufferline'.setup()
       local map = vim.keymap.set
-      -- Remap default (fallback & native) tab mappings
+
       map(
         'n', '<S-h>', '<Cmd>BufferLineCyclePrev<Cr>',
         { desc = 'Move prev tab' }
       )
+
       map(
         'n', '<S-l>', '<Cmd>BufferLineCycleNext<Cr>',
         { desc = 'Move next tab' }
