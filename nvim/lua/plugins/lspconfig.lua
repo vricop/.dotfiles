e6abe2e14fc2850ebd0cd056047d26bdb7687776
Víctor Rico <vricop@gmail.com>
2024-01-09 11:48:24 +0100

    nvim: Update null-ls config file

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,6 +1,6 @@
 return {
   {
-    'jose-elias-alvarez/null-ls.nvim',
+    'nvimtools/none-ls.nvim',
     dependencies = { 'jose-elias-alvarez/typescript.nvim' },
     event = { 'BufReadPre', 'BufNewFile' },
     config = function()
