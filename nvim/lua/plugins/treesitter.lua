41544cf8327b608adb57c65e679ca0c460d95736
Víctor Rico <vricop@gmail.com>
2024-01-09 11:54:23 +0100

    nvim: Update treesitter objects
    Expand treesitter objects for more powerfull actions

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,40 +1,65 @@
 return {
 	{
-		'nvim-treesitter/nvim-treesitter',
-		build = ':TSUpdate',
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter-textobjects",
+		},
 		config = function()
-      require 'nvim-treesitter.configs'.setup {
+			local treesitter = require("nvim-treesitter.configs")
+
+			treesitter.setup({
 				ensure_installed = {
-					'javascript',
-					'typescript',
-					'tsx',
-					'css',
-					'html',
-					'lua',
-					'rust',
+					"javascript",
+					"typescript",
+					"tsx",
+					"css",
+					"html",
+					"lua",
+					"rust",
+					"yaml",
+					"toml",
+					"markdown",
+					"vim",
+					"gitignore",
+					"bash",
 				},
+				-- Improves indent formatting for the `=` command
+				indent = { enable = true },
+				highlight = { enable = true },
 				auto_install = true,
 				sync_install = false,
-				highlight = { enable = true },
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = "<C-space>",
+						node_incremental = "<C-space>",
+						scope_incremental = false,
+						node_decremental = "<bs>",
+					},
+				},
+			})
+
+			-- Skip backwards compatibility routines and speed up loading.
+			vim.g.skip_ts_context_commentstring_module = true
+
+			require("ts_context_commentstring").setup({
 				context_commentstring = {
 					enable = true,
 					enable_autocmd = false,
-          config = {
-            -- Languages that have a single comment style
-            typescript = "// %s",
-            css = "/* %s */",
-            scss = "/* %s */",
-            html = "<!-- %s -->",
-            svelte = "<!-- %s -->",
-            vue = "<!-- %s -->",
-            json = "",
-          },
+					config = {
+						-- Languages that have a single comment style
+						typescript = "// %s",
+						css = "/* %s */",
+						scss = "/* %s */",
+						html = "<!-- %s -->",
+						svelte = "<!-- %s -->",
+						vue = "<!-- %s -->",
+						json = "",
+					},
 				},
-        -- Improves indent formatting for the `=` command
-        indent = {
-          enable = true
-        }
-			}
+			})
 		end,
 	},
 }
