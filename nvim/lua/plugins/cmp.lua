69c55f61e605ee304e62afd2775cb4d695fe4135
Víctor Rico <vricop@gmail.com>
2024-01-09 11:53:15 +0100

    nvim: Update cmp
    - Install tailwind plugin for cmp
    - Update icons

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -1,24 +1,24 @@
 return {
 	{
-		'hrsh7th/nvim-cmp',
+		"hrsh7th/nvim-cmp",
 		dependencies = {
-			'L3MON4D3/LuaSnip',
-			'hrsh7th/cmp-buffer',
-			'hrsh7th/cmp-cmdline',
-			'hrsh7th/cmp-nvim-lsp',
-			'hrsh7th/cmp-path',
-			'saadparwaiz1/cmp_luasnip',
+			"L3MON4D3/LuaSnip",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-cmdline",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-path",
+			"saadparwaiz1/cmp_luasnip",
 			-- 'neovim/nvim-lspconfig',
 		},
 		config = function()
-			local cmp = require('cmp')
+			local cmp = require("cmp")
 
-			require('cmp').setup({
+			require("cmp").setup({
 				experimental = { ghost_text = true },
 				snippet = {
 					-- REQUIRED - you must specify a snippet engine
 					expand = function(args)
-						require('luasnip').lsp_expand(args.body)
+						require("luasnip").lsp_expand(args.body)
 					end,
 				},
 				window = {
@@ -26,96 +26,98 @@ return {
 					documentation = cmp.config.window.bordered(),
 				},
 				mapping = cmp.mapping.preset.insert({
-					['<C-b>'] = cmp.mapping.scroll_docs(-4),
-					['<C-f>'] = cmp.mapping.scroll_docs(4),
-					['<C-X><C-O>'] = cmp.mapping.complete(),
-					['<C-e>'] = cmp.mapping.abort(),
-					['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-X><C-O>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 				}),
 				sources = cmp.config.sources({
-					{ name = 'nvim_lsp' },
-					{ name = 'luasnip' }, -- For luasnip users.
-					{ name = 'path' },
-				}, {
-					{ name = 'buffer' },
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" }, -- For luasnip users.
+					{ name = "path" },
+					{ name = "buffer" },
 				}),
 				-- https://github.com/hrsh7th/nvim-cmp/wiki/Menu-Appearance
 				formatting = {
 					format = function(entry, vim_item)
 						local kind_icons = {
-							Text = '',
-							Color = '',
-							Method = '',
-							Function = '',
-							Constructor = '',
-							Field = '',
-							Variable = '',
-							Class = '',
-							Interface = '',
-							Module = '',
-							Property = '',
-							Unit = '',
-							Value = '',
-							Enum = '',
-							Keyword = '',
-							Snippet = '',
-							File = '',
-							Reference = '',
-							Folder = '',
-							EnumMember = '',
-							Constant = '',
-							Struct = '',
-							Event = '',
-							Operator = '',
-							TypeParameter = '',
+							Text = "",
+							Color = "",
+							Method = "",
+							Function = "",
+							Constructor = "",
+							Field = "",
+							Variable = "",
+							Class = "",
+							Interface = "",
+							Module = "",
+							Property = "",
+							Unit = "",
+							Value = "",
+							Enum = "",
+							Keyword = "",
+							Snippet = "",
+							File = "",
+							Reference = "",
+							Folder = "",
+							EnumMember = "",
+							Constant = "",
+							Struct = "",
+							Event = "",
+							Operator = "",
+							TypeParameter = "",
 						}
 
 						-- Kind icons
 						vim_item.kind = kind_icons[vim_item.kind]
 						-- Source
 						vim_item.menu = ({
-							buffer = ' (Buffer)',
-							nvim_lsp = ' (LSP)',
-							luasnip = ' (LuaSnip)',
-							nvim_lua = ' (Lua)',
-							latex_symbols = ' (LaTeX)',
+							buffer = " (Buffer)",
+							nvim_lsp = " (LSP)",
+							luasnip = " (LuaSnip)",
+							nvim_lua = " (Lua)",
+							latex_symbols = " (LaTeX)",
 						})[entry.source.name]
+
+						require("tailwindcss-colorizer-cmp").formatter(entry, vim_item)
+
 						return vim_item
 					end,
 				},
 			})
 
 			-- Set configuration for specific filetype.
-			cmp.setup.filetype('gitcommit', {
+			cmp.setup.filetype("gitcommit", {
 				sources = cmp.config.sources({
-					{ name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+					{ name = "cmp_git" }, -- You can specify the `cmp_git` source if you were installed it.
 				}, {
-					{ name = 'buffer' },
+					{ name = "buffer" },
 				}),
 			})
 
 			-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-			cmp.setup.cmdline({ '/', '?' }, {
+			cmp.setup.cmdline({ "/", "?" }, {
 				mapping = cmp.mapping.preset.cmdline(),
 				sources = {
-					{ name = 'buffer' },
+					{ name = "buffer" },
 				},
 			})
 
 			-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-			cmp.setup.cmdline(':', {
+			cmp.setup.cmdline(":", {
 				mapping = cmp.mapping.preset.cmdline(),
 				sources = cmp.config.sources({
-					{ name = 'path' },
+					{ name = "path" },
 				}, {
-					{ name = 'cmdline' },
+					{ name = "cmdline" },
 				}),
 			})
 
 			-- Diagnostic icons
-			local signs = { Error = ' ', Warn = ' ', Hint = ' ', Info = ' ' }
+			local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 			for type, icon in pairs(signs) do
-				local hl = 'DiagnosticSign' .. type
+				local hl = "DiagnosticSign" .. type
 				vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 			end
 		end,
