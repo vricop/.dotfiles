f9d6e2aca8f448df46dcd70ba0c002a48b156aeb
Víctor Rico <vricop@gmail.com>
2024-03-10 22:22:34 +0100

    nvim: Fix typo

--- nvim/ftdetect/bash.lua
+++ nvim/ftdetect/bash.lua
@@ -1,4 +1,4 @@
-local group = vim.api.nvim_create_augroup("EnvVariablesTypes", { clear = true })
+local group = vim.api.nvim_create_augroup("EnvVariableTypes", { clear = true })
 
 vim.api.nvim_create_autocmd({ "BufEnter" }, {
   pattern = { ".env.*" },
