0895af978d1ac0c86200a3f1b8d87f929b36222b
Víctor Rico <vricop@gmail.com>
2024-05-19 16:41:28 +0200

    nvim: Update todo comments config

--- nvim/lua/plugins/todo-comments.lua
+++ nvim/lua/plugins/todo-comments.lua
@@ -4,6 +4,13 @@ return {
       'folke/todo-comments.nvim',
       requires = 'nvim-lua/plenary.nvim',
       config = function()
+        local keymap = require('core.utils').keymap
+
+        keymap('n', '<Leader>fT', ':TodoTelescope<Cr>', {
+          desc = "Open TODO items in Telescope",
+          silent = true
+        })
+
         require 'todo-comments'.setup {
           keywords = {
             FIX = {
@@ -15,8 +22,8 @@ return {
             HACK = { icon = ' ', color = 'warning' },
             WARN = { icon = ' ', color = 'warning', alt = { 'WARNING', 'XXX' } },
             PERF = { icon = '󰾆 ', alt = { 'OPTIM', 'PERFORMANCE', 'OPTIMIZE' } },
-            NOTE = { icon = ' ', color = 'hint', alt = { 'INFO' } },
-            TEST = { icon = '󰙨 ', color = 'test', alt = { 'TESTING', 'PASSED', 'FAILED' } },
+            NOTE = { icon = '󰠮 ', color = 'hint', alt = { 'INFO' } },
+            TEST = { icon = '󰙨', color = 'test', alt = { 'TESTING', 'PASSED', 'FAILED' } },
           }
         }
       end
