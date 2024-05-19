56c84ac0bc885e26975db6b865ea758455babdee
Víctor Rico <vricop@gmail.com>
2024-05-19 13:38:36 +0200

    nvim: Update bufferline config
    Use tabs mode. I also added some cool mappings to cycle through tabs and
    buffers. This is how neovim is supposed to handle tabs. Once you get
    used to it's very powerful.

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -1,20 +1,18 @@
 return {
-  {
-    'akinsho/bufferline.nvim',
-    dependencies = { { 'nvim-tree/nvim-web-devicons' } },
-    config = function()
-      require 'bufferline'.setup()
-      local map = vim.keymap.set
+  'akinsho/bufferline.nvim',
+  enabled = true,
+  dependencies = { { 'nvim-tree/nvim-web-devicons' } },
+  config = function()
+    require 'bufferline'.setup({
+      options = {
+        mode = 'tabs',
+      }
+    })
+    local map = vim.keymap.set
 
-      map(
-        'n', '<S-h>', '<Cmd>BufferLineCyclePrev<Cr>',
-        { desc = 'Move prev tab' }
-      )
-
-      map(
-        'n', '<S-l>', '<Cmd>BufferLineCycleNext<Cr>',
-        { desc = 'Move next tab' }
-      )
-    end
-  }
+    map(
+      'n', '<Leader>b', '<Cmd>BufferLinePick<Cr>',
+      { desc = 'Jump to tab' }
+    )
+  end
 }
