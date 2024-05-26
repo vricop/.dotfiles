5f30796c32dbed321e964ad827eed70fb0482692
Víctor Rico <vricop@gmail.com>
2024-05-26 18:05:16 +0200

    nvim: Update higlight-colors config
    - Show virtual color indicator at the end of the word 'eow'
    - Update plugings

--- nvim/lua/plugins/highlight-colors.lua
+++ nvim/lua/plugins/highlight-colors.lua
@@ -1,27 +1,28 @@
 return {
   'brenoprata10/nvim-highlight-colors',
-  config = function()
-    require("nvim-highlight-colors").setup {
-      ---Render style
-      ---@type 'background'|'foreground'|'virtual'
-      render = 'virtual',
+  opts = {
+    ---Render style
+    ---@type 'background'|'foreground'|'virtual'
+    render = 'virtual',
 
-      ---Set virtual symbol (requires render to be set to 'virtual')
-      virtual_symbol = '⏺ ',
+    ---Set virtual symbol (requires render to be set to 'virtual')
+    virtual_symbol = '●',
+    virtual_symbol_prefix = ' ',
+    ---@type 'inline'|'eol'|'eow'
+    virtual_symbol_position = 'eow',
 
-      ---Highlight named colors, e.g. 'green'
-      enable_named_colors = true,
+    ---Highlight named colors, e.g. 'green'
+    enable_named_colors = true,
 
-      ---Highlight tailwind colors, e.g. 'bg-blue-500'
-      enable_tailwind = false,
+    ---Highlight tailwind colors, e.g. 'bg-blue-500'
+    enable_tailwind = false,
 
-      ---Set custom colors
-      ---Label must be properly escaped with '%' to adhere to `string.gmatch`
-      --- :help string.gmatch
-      custom_colors = {
-        { label = '%-%-theme%-primary%-color',   color = '#0f1219' },
-        { label = '%-%-theme%-secondary%-color', color = '#5a5d64' },
-      }
+    ---Set custom colors
+    ---Label must be properly escaped with '%' to adhere to `string.gmatch`
+    --- :help string.gmatch
+    custom_colors = {
+      { label = '%-%-theme%-primary%-color',   color = '#0f1219' },
+      { label = '%-%-theme%-secondary%-color', color = '#5a5d64' },
     }
-  end
+  }
 }
