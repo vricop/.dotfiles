a2c0805af6ff38780a323034cf80f4243fe9fc82
Víctor Rico <vricop@gmail.com>
2024-07-13 19:01:14 +0200

    nvim: Update whichkey

--- nvim/lua/plugins/whichkey.lua
+++ nvim/lua/plugins/whichkey.lua
@@ -1,30 +1,30 @@
 return {
   {
-    "folke/which-key.nvim",
+    'folke/which-key.nvim',
     config = function()
       vim.o.timeout = true
       vim.o.timeoutlen = 300
       local which_key = require 'which-key'
+
       which_key.setup {
+        preset = 'helix',
         icons = {
-          breadcrumb = "",
-          separator = "",
-          group = "",
+          rules = false,
+          breadcrumb = '',
+          separator = '',
+          group = '',
         },
       }
 
-    -- Only add label for groups, descriptions for mapping are set right where
-    -- they're defined
-    which_key.register {
-      ["<Leader>f"] = { name = "Find" },
-      ["<Leader>fg"] = { name = "Find git" },
-      ["<Leader>g"] = { name = "Git" },
-      ["<Leader>l"] = { name = "Lsp" },
-      ["<Leader>t"] = { name = "Toggle" },
-      ["<Leader>tg"] = { name = "Toggle git" },
-      ["<Leader>b"] = { name = "Buffer" },
-      ["gs"] = { name = "Go swap word" },
-    }
+      which_key.add {
+        { '<Leader>b', group = 'Buffer' },
+        { '<Leader>f', group = 'Find' },
+        { '<Leader>fg', group = 'Find git' },
+        { '<Leader>g', group = 'Git' },
+        { '<Leader>l', group = 'Lsp' },
+        { '<Leader>t', group = 'Toggle' },
+        { '<Leader>tg', group = 'Toggle git' },
+      }
     end,
   },
 }
