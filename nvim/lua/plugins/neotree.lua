6ac6fa32259820db37a004171a5356c4dfe40049
Víctor Rico <vricop@gmail.com>
2024-07-13 19:00:38 +0200

    nvim: Add neo-tree & disable nvim-tree

---
+++ nvim/lua/plugins/neotree.lua
@@ -0,0 +1,49 @@
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  branch = 'v3.x',
+  enabled = true,
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  keys = {
+    {
+      '<Leader>e',
+      ':Neotree toggle<Cr>',
+      desc = 'Open Neotree',
+      noremap = true,
+      silent = true,
+    },
+  },
+  config = function()
+    require('neo-tree').setup {
+      window = {
+        mappings = {
+          ['l'] = 'open',
+          ['h'] = 'close_node',
+          ['<Space>'] = 'none',
+          ['[c'] = 'prev_git_modified',
+          [']c'] = 'next_git_modified'
+        },
+      },
+      default_component_configs = {
+        git_status = {
+          symbols = {
+            -- Change type
+            added = '󰐕 ',
+            deleted = '󰍴 ',
+            modified = ' ',
+            renamed = 'R',
+            -- Status type
+            untracked = '?',
+            ignored = ' ',
+            unstaged = '',
+            staged = '',
+            conflict = ' ',
+          },
+        },
+      },
+    }
+  end,
+}
--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -2,6 +2,7 @@ return {
   {
     "nvim-tree/nvim-tree.lua",
     dependencies = { { "nvim-tree/nvim-web-devicons" } },
+    enabled = false,
     config = function()
       local keymap = require 'core.utils'.keymap
       require('nvim-tree').setup({
