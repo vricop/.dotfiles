93769f8c2a5e89c527b0dddc9cf400945f8ec63b
Víctor Rico <vricop@gmail.com>
2024-08-25 12:31:07 +0200

    nvim: Fix typo in autogroup

--- nvim/ftdetect/keymap.lua
+++ nvim/ftdetect/keymap.lua
@@ -1,4 +1,4 @@
-local group = vim.api.nvim_create_augroup("KeyampFileType", { clear = true })
+local group = vim.api.nvim_create_augroup("KeymapFileType", { clear = true })
 
 vim.api.nvim_create_autocmd({ "BufEnter" }, {
   pattern = { "*.keymap" },
