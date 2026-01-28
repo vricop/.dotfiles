c4d441a225951828f40b72efc75f70a9e5de166e
Víctor Rico <vricop@gmail.com>
2026-01-28 13:14:10 +0100

    feat(nvim): Add autocmd for cli integrations

--- nvim/lua/autocmds/init.lua
+++ nvim/lua/autocmds/init.lua
@@ -7,3 +7,19 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
     vim.bo.filetype = "conf"
   end,
 })
+
+---Close term after pressing `q` `esc` or `Ctrl-c`
+local function auto_close_term(cmd_pattern)
+  vim.api.nvim_create_autocmd("TermClose", {
+    pattern = cmd_pattern,
+    callback = function(args)
+      vim.schedule(function()
+        vim.api.nvim_buf_delete(args.buf, { force = true })
+      end)
+    end,
+  })
+end
+
+auto_close_term("*opencode*")
+auto_close_term("*lazygit*")
+auto_close_term("*serpl*")
